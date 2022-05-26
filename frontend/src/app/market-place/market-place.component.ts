bc9cd037833f41b800d26184332e63af9ccad666
GoncaloGil0 <goncalopedrogil@gmail.com>
2022-05-26 13:29:42 +0100

    programação dos dropdowns e interação com os btns

--- frontend/src/app/market-place/market-place.component.ts
+++ frontend/src/app/market-place/market-place.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Book } from '../services/rest/models/book';
-import { BookService } from '../services/rest/book/book.service';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Book } from '../services/rest/models/book'
+//import { BookService } from '../services/rest/book/book.service';
 
 @Component({
   selector: 'app-market-place',
   templateUrl: './market-place.component.html',
-  styleUrls: ['./market-place.component.css']
+  styleUrls: ['./market-place.component.css'],
 })
 export class MarketPlaceComponent implements OnInit {
-  book!: Book;
+  /*book!: Book;
+public rest:BookService, private route: ActivatedRoute, private router: Router*/
+  constructor() {}
 
-  constructor(public rest:BookService, private route: ActivatedRoute, private router: Router) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
+  page = "Market Place"
 
+  dropedAutor:string = "droped inactive";
+  changeDropedAutor(){ this.dropedAutor == "droped inactive" ? this.dropedAutor = "droped" : this.dropedAutor = "droped inactive";}
+
+
+  autorLabel:string = "Autor";
+  changeAutorLabel(autor:string){ this.autorLabel = autor;}
+
+  /*
   getAllBooks(){
     this.rest.getAllBooks().subscribe((data: any) => {
     this.book = data;
@@ -27,5 +36,5 @@ export class MarketPlaceComponent implements OnInit {
     this.rest.getBook(filter).subscribe((data: any) => {
     this.book = data;
     })
-  }
+  }*/
 }
