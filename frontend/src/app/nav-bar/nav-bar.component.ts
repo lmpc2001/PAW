28ecdd978946aa7d2ae59715b7c25c110861023c
GoncaloGil0 <goncalopedrogil@gmail.com>
2022-05-26 13:30:13 +0100

    programação da mudança de aba dependendo da pagina

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core'
-
+import { Component, Input, OnInit } from '@angular/core'
 interface linkProps {
   name: string
   to: string
@@ -10,9 +9,19 @@ interface linkProps {
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css'],
 })
-
 export class NavBarComponent implements OnInit {
+  @Input() page: String | undefined
+
   constructor() {}
+
+  getPage(linkName:string) {
+    switch (linkName){
+      case this.page:
+        return "linksNav active";
+      default:
+        return "linksNav";
+    }
+  }
 
   link1: linkProps = {
     name: 'DashBoard',
